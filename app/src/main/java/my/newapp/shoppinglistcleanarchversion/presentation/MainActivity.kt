1bb1c4093faf129ce0647f8b1b1a04b2a35930ea
fsm13 <fomichevsm@gmail.com>
2023-03-26 13:50:38 +0700

    Creating packages

--- app/src/main/java/my/newapp/shoppinglistcleanarchversion/presentation/MainActivity.kt
+++ app/src/main/java/my/newapp/shoppinglistcleanarchversion/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package my.newapp.shoppinglistcleanarchversion
+package my.newapp.shoppinglistcleanarchversion.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import my.newapp.shoppinglistcleanarchversion.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
