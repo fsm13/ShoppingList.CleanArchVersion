f5017d1cb6b5e4bd820f5e03134fdf44e737134a
fsm13 <fomichevsm@gmail.com>
2023-03-26 18:14:46 +0700

    Creating data module

--- app/src/main/java/my/newapp/shoppinglistcleanarchversion/domain/ShopItem.kt
+++ app/src/main/java/my/newapp/shoppinglistcleanarchversion/domain/ShopItem.kt
@@ -3,8 +3,12 @@ package my.newapp.shoppinglistcleanarchversion.domain
 import android.os.strictmode.UntaggedSocketViolation
 
 data class ShopItem(
-    val id: Int,
     val name: String,
     val count: Int,
-    val enabled: Boolean
-)
+    val enabled: Boolean,
+    var id: Int = UNDEFINED_ID
+) {
+    companion object {
+        const val UNDEFINED_ID = -1
+    }
+}
