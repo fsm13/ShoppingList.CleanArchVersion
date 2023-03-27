477bf541f4a38cd78305028ae826b74c4f1d8b29
fsm13 <fomichevsm@gmail.com>
2023-03-27 11:32:57 +0700

    Creating presentation module

--- app/src/main/java/my/newapp/shoppinglistcleanarchversion/presentation/MainActivity.kt
+++ app/src/main/java/my/newapp/shoppinglistcleanarchversion/presentation/MainActivity.kt
@@ -2,11 +2,17 @@ package my.newapp.shoppinglistcleanarchversion.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import my.newapp.shoppinglistcleanarchversion.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel:  MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this) [MainViewModel::class.java]
+        viewModel.shopList.observe(this) {
+
+        }
     }
 }
