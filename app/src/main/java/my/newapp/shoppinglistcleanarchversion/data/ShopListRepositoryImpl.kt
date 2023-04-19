27382cebc6c2c3131dfbab9eabc33d3032d7dadf
fsm13 <fomichevsm@gmail.com>
2023-04-19 11:50:33 +0700

    add dependencies

--- app/src/main/java/my/newapp/shoppinglistcleanarchversion/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/my/newapp/shoppinglistcleanarchversion/data/ShopListRepositoryImpl.kt
@@ -12,12 +12,12 @@ object ShopListRepositoryImpl: ShopListRepository {
     private val shopList = sortedSetOf<ShopItem>({ o1, o2 -> o1.id.compareTo(o2.id)})
     private var autoIncrementId = 0
 
-    init {
+    /*init {
         for (i in 0 until 100) {
             val item = ShopItem("Name $i", i, Random.nextBoolean())
             addShopItem(item)
         }
-    }
+    }*/
 
 
     override fun addShopItem(shopItem: ShopItem) {
