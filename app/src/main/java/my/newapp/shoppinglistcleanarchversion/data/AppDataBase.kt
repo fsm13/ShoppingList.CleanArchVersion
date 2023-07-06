561bf62022a5514a1bc4910a4237f104df6664a1
fsm13 <fomichevsm@gmail.com>
2023-07-06 15:20:00 +0300

    fix

--- app/src/main/java/my/newapp/shoppinglistcleanarchversion/data/AppDataBase.kt
+++ app/src/main/java/my/newapp/shoppinglistcleanarchversion/data/AppDataBase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 
 @Database(entities = [ShopItemDbModel::class], version = 1, exportSchema = false)
 abstract class AppDataBase : RoomDatabase() {
-
+/////////////////////////////////////
     abstract fun shopListDao(): ShopListDao
 
     companion object {
